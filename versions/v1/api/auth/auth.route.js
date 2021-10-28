89cc27e81537bb9c2edebfa2c97f8e4c9f203dfc
aawezk456 <aawezk456@gmail.com>
2021-10-28 16:46:45 +0530

    change method of http

--- versions/v1/api/auth/auth.route.js
+++ versions/v1/api/auth/auth.route.js
@@ -15,7 +15,7 @@ router.post("/login", signIn);
 router.post("/register", signUp);
 router.post("/forgotPassword", forgotPassword);
 router.put("/resetPassword/:resetToken", resetPassword);
-router.put("/changeName/:id", changeName);
-router.put("/changePassword/:id", changePassword);
+router.patch("/changeName/:id", changeName);
+router.patch("/changePassword/:id", changePassword);
 
 module.exports = router;
