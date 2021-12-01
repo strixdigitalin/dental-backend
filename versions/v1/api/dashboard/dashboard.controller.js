fc84f233f5bf36fa2e5a2bf703a4baf7d927187e
junaidfkhan786 <junaidfkhan786@gmail.com>
2021-12-01 16:49:23 +0530

    update : used info api

--- versions/v1/api/dashboard/dashboard.controller.js
+++ versions/v1/api/dashboard/dashboard.controller.js
@@ -19,8 +19,23 @@ exports.getUsedInfo = async (req, res, next) => {
 
         },
         function (callback) {
-            Profile.findOne({ user: req.user.id }).exec((err, result) => {
-                callback(err, result.question_details.length)
+            Profile.findOne({ user: req.user.id }).populate('user').exec((err, result) => {
+                var total_correct = 0
+                var total_incorrect = 0
+                result.question_details.forEach(element => {
+                    if (element.isCorrect) {
+                        total_correct++
+                    }
+                    if (element.isIncorrect) {
+                        total_incorrect++
+                    }
+                });
+                callback(err, {
+                    user_detail: { name: result.user.firstName + ' ' + result.user.lastName },
+                    used: result.question_details.length,
+                    total_correct: total_correct,
+                    total_incorrect: total_incorrect
+                })
             })
         },
         function (callback) {
@@ -33,14 +48,17 @@ exports.getUsedInfo = async (req, res, next) => {
         const [question, used_question, test] = result
         let question_detail = {
             count: question,
-            used: used_question,
-            unused: question - used_question
+            used: used_question.used,
+            unused: question - used_question.used,
+            total_correct: used_question.total_correct,
+            total_incorrect: used_question.total_incorrect
         }
         let test_detail = {
             test_created: test,
             test_completed: test
         }
         let response = {
+            user_detail: used_question.user_detail,
             question_detail,
             test_detail
         }
